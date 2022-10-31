791bbdd56eb4e3d674507847f5f282489c51378c
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-31 23:57:31 +0200

    додавання розмітка

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import { fetchCountries } from './news-service/countries';
+import { fetchCountries } from './countries.js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputField = document.querySelector('#search-box');
+const listCountry = document.querySelector('.country-list');
+const infoCountry = document.querySelector('.country-info');
+
+inputField.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(e) {
+  infoCountry.innerHTML = '';
+  listCountry.innerHTML = '';
+
+  fetchCountries(e.target.value).then(setCounries => {
+    return setCounries;
+  });
+}
+function addCountryOnList(setCounries) {
+  return setCounries
+    .map(({ name, flags: { svg } }) => {
+      `<li class='country'>
+      <img src="${svg}" alt="" width="100">
+      <p class="name-countries">${name}</p>
+    </li>
+    `;
+    })
+    .join('');
+}
