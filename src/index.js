1e71e46b14955adaac0841e32cfe86bd16b6b3da
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-31 15:44:52 +0200

    прописання імортів

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './news-service/countries';
 
 const DEBOUNCE_DELAY = 300;
